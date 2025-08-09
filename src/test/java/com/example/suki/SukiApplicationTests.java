ec3ebbf9276b53ad5e2c4dbf4126de373df1bc7a
isExample <yeji0331@naver.com>
2025-08-09 17:22:49 +0900

    init project

---
+++ src/test/java/com/example/suki/SukiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
