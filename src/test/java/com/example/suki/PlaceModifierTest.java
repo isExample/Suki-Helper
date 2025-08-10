2fff5c997d8be5c9eec388ee7d2c74c9d16eaf59
isExample <yeji0331@naver.com>
2025-08-10 21:42:34 +0900

    test: 활성화 조건부장소 추가 검증

--- src/test/java/com/example/suki/PlaceModifierTest.java
+++ src/test/java/com/example/suki/PlaceModifierTest.java
@@ -3,6 +3,7 @@ package com.example.suki;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlaceModifierTest {
     @Test
@@ -10,8 +11,18 @@ public class PlaceModifierTest {
         PlaceModifier modifier = new PlaceModifier();
         UserState userState = new UserState();
 
-        modifier.modify(userState, PlaceCategory.SCHOOL);
+        modifier.modify(userState, PlaceCategory.SCHOOL, PlaceCategory.GOLD_MINE);
 
         assertFalse(userState.getPlaces().containsKey(PlaceCategory.SCHOOL));
+    }
+
+    @Test
+    void 주어진_활성화된_조건부장소는_유저상태에_추가된다(){
+        PlaceModifier modifier = new PlaceModifier();
+        UserState userState = new UserState();
+
+        modifier.modify(userState, PlaceCategory.SCHOOL, PlaceCategory.GOLD_MINE);
+
+        assertTrue(userState.getPlaces().containsKey(PlaceCategory.GOLD_MINE));
     }
 }
