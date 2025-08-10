3d71387e3eff1b1d9763d82fc5e172566156805e
isExample <yeji0331@naver.com>
2025-08-10 23:14:19 +0900

    refactor: 인스턴스 초기화 중복 제거

--- src/test/java/com/example/suki/FitnessLevelModifierTest.java
+++ src/test/java/com/example/suki/FitnessLevelModifierTest.java
@@ -1,5 +1,6 @@
 package com.example.suki;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -7,20 +8,23 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class FitnessLevelModifierTest {
+    private FitnessLevelModifier modifier;
+    private UserState userState;
+
+    @BeforeEach
+    void setUp(){
+        modifier = new FitnessLevelModifier();
+        userState = new UserState();
+    }
+
     @ParameterizedTest
     @CsvSource({"-1", "11", "100"})
     void 범위밖_운동레벨은_예외를_발생시킨다(int level){
-        FitnessLevelModifier modifier = new FitnessLevelModifier();
-        UserState userState = new UserState();
-
         assertThrows(IllegalArgumentException.class, () -> modifier.modify(userState, level));
     }
 
     @Test
     void 운동레벨이_최대치면_운동하기_행동은_비활성화된다(){
-        FitnessLevelModifier modifier = new FitnessLevelModifier();
-        UserState userState = new UserState();
-
         modifier.modify(userState, 10);
 
         for(Place place : userState.getPlaces().values()) {
