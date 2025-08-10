ffc58ebda6cde0afa1a970352de5941de4496928
isExample <yeji0331@naver.com>
2025-08-10 22:54:12 +0900

    test: 운동레벨 최대치인 경우 운동하기 비활성화 검증
    - 운동레벨 최대치는 10
    - 모든 장소의 운동하기 제거

--- src/test/java/com/example/suki/FitnessLevelModifierTest.java
+++ src/test/java/com/example/suki/FitnessLevelModifierTest.java
@@ -1,9 +1,10 @@
 package com.example.suki;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FitnessLevelModifierTest {
     @ParameterizedTest
@@ -13,5 +14,18 @@ public class FitnessLevelModifierTest {
         UserState userState = new UserState();
 
         assertThrows(IllegalArgumentException.class, () -> modifier.modify(userState, level));
+    }
+
+    @Test
+    void 운동레벨이_최대치면_운동하기_행동은_비활성화된다(){
+        FitnessLevelModifier modifier = new FitnessLevelModifier();
+        UserState userState = new UserState();
+
+        modifier.modify(userState, 10);
+
+        for(Place place : userState.getPlaces().values()) {
+            assertTrue(place.getActions().containsKey(ActionCategory.STUDY));       // 다른 기본행동 존재 확인
+            assertFalse(place.getActions().containsKey(ActionCategory.EXERCISE));   // 운동하기 비활성화
+        }
     }
 }
