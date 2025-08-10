b036ca30a8c02ef2cdeb91039b1aa39b846dcfd0
isExample <yeji0331@naver.com>
2025-08-10 23:28:41 +0900

    test: 운동레벨에 따른 체력 보정 검증
    - 운동레벨 1 당 행동 체력지수 1 증가

--- src/test/java/com/example/suki/FitnessLevelModifierTest.java
+++ src/test/java/com/example/suki/FitnessLevelModifierTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +19,11 @@ public class FitnessLevelModifierTest {
     void setUp(){
         modifier = new FitnessLevelModifier();
         userState = new UserState();
+    }
+
+    static Stream<PlaceCategory> 기본장소() {
+        return Arrays.stream(PlaceCategory.values())
+                .filter(PlaceCategory::isDefault);
     }
 
     @ParameterizedTest
@@ -30,6 +39,20 @@ public class FitnessLevelModifierTest {
         for(Place place : userState.getPlaces().values()) {
             assertTrue(place.getActions().containsKey(ActionCategory.STUDY));       // 다른 기본행동 존재 확인
             assertFalse(place.getActions().containsKey(ActionCategory.EXERCISE));   // 운동하기 비활성화
+        }
+    }
+
+    @ParameterizedTest
+    @MethodSource("기본장소")
+    void 운동레벨_1당_행동_체력지수가_1_증가한다(PlaceCategory placeCategory){
+        int fitnessLevel = 5;
+        Place place = userState.getPlaces().get(placeCategory);
+
+        modifier.modify(userState, fitnessLevel);
+
+        for(ActionCategory action : place.getActions().keySet()) {
+            int baseStamina = placeCategory.getActions().get(action);
+            assertEquals(baseStamina + fitnessLevel, place.getActions().get(action));
         }
     }
 }
