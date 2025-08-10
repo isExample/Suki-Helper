3b2ea77ce8181ee99c7fbf8cbc9a6b7d2d2fee62
isExample <yeji0331@naver.com>
2025-08-10 17:31:44 +0900

    feat: 행동 enum에 기본체력 필드 추가

--- src/main/java/com/example/suki/ActionCategory.java
+++ src/main/java/com/example/suki/ActionCategory.java
@@ -1,8 +1,15 @@
 package com.example.suki;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ActionCategory {
-    STUDY,          // 공부하기
-    PART_TIME,      // 알바하기
-    EXERCISE,       // 운동하기
-    SLEEP           // 잠자기
+    STUDY(-16),          // 공부하기
+    PART_TIME(-29),      // 알바하기
+    EXERCISE(-24),       // 운동하기
+    SLEEP(10);           // 잠자기
+
+    private final int stamina;
 }
