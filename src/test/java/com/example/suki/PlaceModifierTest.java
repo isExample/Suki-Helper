9212fdcad1153afb546685ad066dbfb0e5e6715d
isExample <yeji0331@naver.com>
2025-08-10 21:57:56 +0900

    refactor: 인스턴스 생성 중복 제거

--- src/test/java/com/example/suki/PlaceModifierTest.java
+++ src/test/java/com/example/suki/PlaceModifierTest.java
@@ -1,5 +1,6 @@
 package com.example.suki;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,11 +9,17 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlaceModifierTest {
+    private PlaceModifier modifier;
+    private UserState userState;
+
+    @BeforeEach
+    void setUp(){
+        modifier = new PlaceModifier();
+        userState = new UserState();
+    }
+
     @Test
     void 주어진_비활성화된_기본장소는_유저상태에서_제거된다(){
-        PlaceModifier modifier = new PlaceModifier();
-        UserState userState = new UserState();
-
         modifier.modify(userState, List.of(PlaceCategory.SCHOOL), List.of());
 
         assertFalse(userState.getPlaces().containsKey(PlaceCategory.SCHOOL));
@@ -20,9 +27,6 @@ public class PlaceModifierTest {
 
     @Test
     void 주어진_활성화된_조건부장소는_유저상태에_추가된다(){
-        PlaceModifier modifier = new PlaceModifier();
-        UserState userState = new UserState();
-
         modifier.modify(userState, List.of(), List.of(PlaceCategory.GOLD_MINE));
 
         assertTrue(userState.getPlaces().containsKey(PlaceCategory.GOLD_MINE));
