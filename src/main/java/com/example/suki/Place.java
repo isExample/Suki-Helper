2b1a721eb3fe3ed8f7d06c4676037387b955bc66
isExample <yeji0331@naver.com>
2025-08-10 19:40:17 +0900

    feat: 장소 초기화시 null 체크 추가

--- src/main/java/com/example/suki/Place.java
+++ src/main/java/com/example/suki/Place.java
@@ -2,14 +2,16 @@ package com.example.suki;
 
 import lombok.Getter;
 
-import java.util.EnumMap;
 import java.util.Map;
 
 @Getter
 public class Place {
     private final Map<ActionCategory, Integer> actions;
 
-    public Place(PlaceCategory category) {
-        this.actions = Map.copyOf(category.getActions());
+    public Place(PlaceCategory placeCategory) {
+        if(placeCategory == null) {
+            throw new IllegalArgumentException("장소는 null일 수 없습니다.");
+        }
+        this.actions = Map.copyOf(placeCategory.getActions());
     }
 }
