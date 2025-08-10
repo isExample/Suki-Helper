0b5e400e4e1b7338ea3e04481eab63fecb5780b8
isExample <yeji0331@naver.com>
2025-08-10 20:48:35 +0900

    refactor: 장소 enum 순회하며 기본장소 추가
    - 기본장소 추가 하드코딩 제거

--- src/main/java/com/example/suki/UserState.java
+++ src/main/java/com/example/suki/UserState.java
@@ -2,6 +2,7 @@ package com.example.suki;
 
 import lombok.Getter;
 
+import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.Map;
 
@@ -12,10 +13,8 @@ public class UserState {
     public UserState() {
         this.places = new EnumMap<>(PlaceCategory.class);
 
-        this.places.put(PlaceCategory.SCHOOL, new Place(PlaceCategory.SCHOOL));
-        this.places.put(PlaceCategory.HOME, new Place(PlaceCategory.HOME));
-        this.places.put(PlaceCategory.PARK, new Place(PlaceCategory.PARK));
-        this.places.put(PlaceCategory.CAFE, new Place(PlaceCategory.CAFE));
-        this.places.put(PlaceCategory.LIBRARY, new Place(PlaceCategory.LIBRARY));
+        Arrays.stream(PlaceCategory.values())
+                .filter(PlaceCategory::isDefault)
+                .forEach(category -> this.places.put(category, new Place(category)));
     }
 }
