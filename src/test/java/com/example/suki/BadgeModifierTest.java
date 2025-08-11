952846010c0754c4d3c50a98bb4162756bbf5c06
isExample <yeji0331@naver.com>
2025-08-11 16:17:35 +0900

    test: 잠자기특화 장소와 뱃지 모두 검증
    - 테스트 일반화

--- src/test/java/com/example/suki/BadgeModifierTest.java
+++ src/test/java/com/example/suki/BadgeModifierTest.java
@@ -1,20 +1,40 @@
 package com.example.suki;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BadgeModifierTest {
-    @Test
-    void 잠자기특화장소의_잠자기_체력지수를_증가시키는_뱃지가_존재한다(){
+    static Stream<Arguments> 잠자기특화_장소x뱃지() {
+        List<PlaceCategory> sleepPlaces =
+                Arrays.stream(PlaceCategory.values())
+                        .filter(p -> p.isSpecializedFor(ActionCategory.SLEEP))
+                        .toList();
+
+        return Arrays.stream(BadgeCategory.values())
+                .filter(b -> b.getActionCategory() == ActionCategory.SLEEP)
+                .flatMap(b -> sleepPlaces.stream().map(p -> Arguments.of(b, p)));
+    }
+
+    @ParameterizedTest
+    @MethodSource("잠자기특화_장소x뱃지")
+    void 잠자기특화장소의_잠자기_체력지수를_증가시키는_뱃지가_존재한다(BadgeCategory badge, PlaceCategory place){
         BadgeModifier modifier = new BadgeModifier();
         UserState userState = new UserState();
-        BadgeCategory badge = BadgeCategory.UNIV_1;
-        int baseStamina = userState.getPlaces().get(PlaceCategory.HOME).getActions().get(ActionCategory.SLEEP);
+        userState.activatePlace(PlaceCategory.ART_GALLERY);
+
+        int baseStamina = userState.getPlaces().get(place).getActions().get(ActionCategory.SLEEP);
         int bonusStamina = badge.getValue();
 
         modifier.modify(userState, badge);
 
-        assertEquals(baseStamina + bonusStamina, userState.getPlaces().get(PlaceCategory.HOME).getActions().get(ActionCategory.SLEEP));
+        assertEquals(baseStamina + bonusStamina, userState.getPlaces().get(place).getActions().get(ActionCategory.SLEEP));
     }
 }
