c7cc6a520801b13e625075d39fe0623b065bdc0c
isExample <yeji0331@naver.com>
2025-08-11 16:37:51 +0900

    refactor: stream 재사용

--- src/test/java/com/example/suki/BadgeModifierTest.java
+++ src/test/java/com/example/suki/BadgeModifierTest.java
@@ -1,7 +1,5 @@
 package com.example.suki;
 
-import org.apache.catalina.User;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,19 +11,24 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BadgeModifierTest {
-    static Stream<Arguments> 잠자기특화_장소x뱃지() {
-        List<PlaceCategory> sleepPlaces =
-                Arrays.stream(PlaceCategory.values())
-                        .filter(p -> p.isSpecializedFor(ActionCategory.SLEEP))
-                        .toList();
+    public static Stream<PlaceCategory> 잠자기특화장소() {
+        return Arrays.stream(PlaceCategory.values())
+                .filter(p -> p.isSpecializedFor(ActionCategory.SLEEP));
+    }
 
+    public static Stream<BadgeCategory> 잠자기특화뱃지() {
         return Arrays.stream(BadgeCategory.values())
-                .filter(b -> b.getActionCategory() == ActionCategory.SLEEP)
+                .filter(b -> b.getActionCategory() == ActionCategory.SLEEP);
+    }
+
+    public static Stream<Arguments> 잠자기특화장소x뱃지() {
+        List<PlaceCategory> sleepPlaces = 잠자기특화장소().toList();
+        return 잠자기특화뱃지()
                 .flatMap(b -> sleepPlaces.stream().map(p -> Arguments.of(b, p)));
     }
 
     @ParameterizedTest
-    @MethodSource("잠자기특화_장소x뱃지")
+    @MethodSource("잠자기특화장소x뱃지")
     void 잠자기특화장소의_잠자기_체력지수를_증가시키는_뱃지가_존재한다(BadgeCategory badge, PlaceCategory place){
         BadgeModifier modifier = new BadgeModifier();
         UserState userState = new UserState();
@@ -39,18 +42,22 @@ public class BadgeModifierTest {
         assertEquals(baseStamina + bonusStamina, userState.getPlaces().get(place).getActions().get(ActionCategory.SLEEP));
     }
 
-    @Test
-    void 잠자기특화뱃지_효과는_중첩된다(){
+    @ParameterizedTest
+    @MethodSource("잠자기특화장소")
+    void 잠자기특화뱃지_효과는_중첩된다(PlaceCategory place){
         BadgeModifier modifier = new BadgeModifier();
         UserState userState = new UserState();
+        userState.activatePlace(PlaceCategory.ART_GALLERY);
 
-        BadgeCategory badge1 = BadgeCategory.UNIV_1;
-        BadgeCategory badge2 = BadgeCategory.UNIV_2;
-        int baseStamina = userState.getPlaces().get(PlaceCategory.HOME).getActions().get(ActionCategory.SLEEP);
-        int totalBonus = badge1.getValue() + badge2.getValue();
 
-        modifier.modify(userState, List.of(badge1, badge2));
+        List<BadgeCategory> badgeList = 잠자기특화뱃지().toList();
+        int baseStamina = userState.getPlaces().get(place).getActions().get(ActionCategory.SLEEP);
+        int totalBonus = badgeList.stream()
+                .mapToInt(BadgeCategory::getValue)
+                .sum();
 
-        assertEquals(baseStamina + totalBonus, userState.getPlaces().get(PlaceCategory.HOME).getActions().get(ActionCategory.SLEEP));
+        modifier.modify(userState, badgeList);
+
+        assertEquals(baseStamina + totalBonus, userState.getPlaces().get(place).getActions().get(ActionCategory.SLEEP));
     }
 }
