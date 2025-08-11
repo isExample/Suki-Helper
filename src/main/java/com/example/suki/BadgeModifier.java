1dabe95b0331a7cbc4b470197a8c96822eaf03a4
isExample <yeji0331@naver.com>
2025-08-11 16:26:05 +0900

    feat: 다중 뱃지 입력 및 중첩 구현

--- src/main/java/com/example/suki/BadgeModifier.java
+++ src/main/java/com/example/suki/BadgeModifier.java
@@ -1,8 +1,12 @@
 package com.example.suki;
 
+import java.util.List;
+
 public class BadgeModifier {
-    public void modify(UserState userState, BadgeCategory badge){
-        int bonus = badge.getValue();
-        userState.applyDeltaToSpecializedPlace(bonus, badge.getActionCategory());
+    public void modify(UserState userState, List<BadgeCategory> badgeList){
+        for(BadgeCategory badge : badgeList){
+            userState.applyDeltaToSpecializedPlace(badge.getValue(), badge.getActionCategory());
+        }
+
     }
 }
