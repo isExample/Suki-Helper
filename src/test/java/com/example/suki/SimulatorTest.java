832d2b993ef365b9013f588dff79504a0e1fddc6
isExample <yeji0331@naver.com>
2025-08-12 22:59:25 +0900

    test: 테스트 케이스 추가

--- src/test/java/com/example/suki/SimulatorTest.java
+++ src/test/java/com/example/suki/SimulatorTest.java
@@ -1,16 +1,18 @@
 package com.example.suki;
 
 import com.example.suki.domain.UserState;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SimulatorTest {
-    @Test
-    void 목표체력은_1_이상_99_이하이다(){
+    @ParameterizedTest
+    @CsvSource({"0", "100"})
+    void 목표체력은_1_이상_99_이하이다(int target){
         Simulator simulator = new Simulator();
         UserState userState = new UserState();
 
-        assertThrows(IllegalArgumentException.class, () -> simulator.simulate(userState, 0));
+        assertThrows(IllegalArgumentException.class, () -> simulator.simulate(userState, target));
     }
 }
