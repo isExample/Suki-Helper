b1939d125b3f48a6e88e355ba4a9f7b7660a3033
isExample <yeji0331@naver.com>
2025-08-12 18:15:38 +0900

    test: 특정행동 특성 효과 검증

--- src/test/java/com/example/suki/TraitModifierTest.java
+++ src/test/java/com/example/suki/TraitModifierTest.java
@@ -2,10 +2,12 @@ package com.example.suki;
 
 import com.example.suki.domain.UserState;
 import com.example.suki.domain.action.ActionCategory;
+import com.example.suki.domain.effect.ActionScope;
 import com.example.suki.domain.effect.All;
 import com.example.suki.domain.place.PlaceCategory;
 import com.example.suki.domain.trait.TraitCategory;
 import com.example.suki.modifier.TraitModifier;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -29,9 +31,20 @@ public class TraitModifierTest {
                 .filter(trait -> trait.getEffect() instanceof All);
     }
 
+    static Stream<TraitCategory> 특정행동_특성(){
+        return Arrays.stream(TraitCategory.values())
+                .filter(trait -> trait.getEffect() instanceof ActionScope);
+    }
+
     public static Stream<Arguments> 모든장소_모든행동_특성x기본장소() {
         List<PlaceCategory> places = 기본장소().toList();
         return 모든장소_모든행동_특성()
+                .flatMap(t -> places.stream().map(p -> Arguments.of(t, p)));
+    }
+
+    public static Stream<Arguments> 특정행동_특성x기본장소() {
+        List<PlaceCategory> places = 기본장소().toList();
+        return 특정행동_특성()
                 .flatMap(t -> places.stream().map(p -> Arguments.of(t, p)));
     }
 
@@ -49,6 +62,21 @@ public class TraitModifierTest {
         before.forEach((action, base) -> {
             assertEquals(base + trait.getEffect().deltaFor(place, action), after.get(action));
         });
+    }
 
+    @ParameterizedTest
+    @MethodSource("특정행동_특성x기본장소")
+    void 특정행동의_체력소모량을_감소시키는_특성이_존재한다(TraitCategory trait, PlaceCategory place){
+        TraitModifier modifier = new TraitModifier();
+        UserState userState = new UserState();
+
+        Map<ActionCategory, Integer> before = new EnumMap<>(userState.getPlaces().get(place).getActions());
+
+        modifier.modify(userState, List.of(trait));
+
+        Map<ActionCategory, Integer> after = userState.getPlaces().get(place).getActions();
+        before.forEach((action, base) -> {
+            assertEquals(base + trait.getEffect().deltaFor(place, action), after.get(action));
+        });
     }
 }
