e0f0a86ce4a5ebce920a07443417893042863c81
isExample <yeji0331@naver.com>
2025-08-12 20:02:04 +0900

    feat: 특성 6개 초과시 예외 발생

--- src/main/java/com/example/suki/modifier/TraitModifier.java
+++ src/main/java/com/example/suki/modifier/TraitModifier.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class TraitModifier {
     public void modify(UserState userState, List<TraitCategory> traitList){
+        if(traitList.size() > 6) throw new IllegalArgumentException("특성은 최대 6개로 제한됩니다.");
+
         for(TraitCategory trait : traitList){
             trait.apply(userState);
         }
