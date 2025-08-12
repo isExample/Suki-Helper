889e86219082e9aa423b3e9b3320fae1e35f6568
isExample <yeji0331@naver.com>
2025-08-12 20:38:25 +0900

    test: 유저상태 요일정보 null 검증

--- src/test/java/com/example/suki/UserStateTest.java
+++ src/test/java/com/example/suki/UserStateTest.java
@@ -3,6 +3,7 @@ package com.example.suki;
 import com.example.suki.domain.place.PlaceCategory;
 import com.example.suki.domain.UserState;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
@@ -33,6 +34,11 @@ public class UserStateTest {
     @MethodSource("com.example.suki.TestSources#조건부장소")
     void 기본_유저상태는_조건부장소를_포함하지_않는다(PlaceCategory placeCategory){
         assertFalse(userState.getPlaces().containsKey(placeCategory));
+    }
+
+    @Test
+    void 기본_유저상태는_요일정보를_가진다(){
+        assertNotNull(userState.getDay());
     }
 
     @ParameterizedTest
