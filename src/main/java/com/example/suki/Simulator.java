fffc9e466835c8e9069a8061ca7419c64b2b53f7
isExample <yeji0331@naver.com>
2025-08-13 17:12:13 +0900

    feat: 주말 목표체력 달성 조합 반환 로직 구현
    - 브루트포스(백트래킹) 방식

--- src/main/java/com/example/suki/Simulator.java
+++ src/main/java/com/example/suki/Simulator.java
@@ -1,9 +1,62 @@
 package com.example.suki;
 
+import com.example.suki.domain.SimulationResult;
+import com.example.suki.domain.Tick;
 import com.example.suki.domain.UserState;
+import com.example.suki.domain.action.ActionCategory;
+import com.example.suki.domain.place.Place;
+import com.example.suki.domain.place.PlaceCategory;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Simulator {
-    public void simulate(UserState userState, int target){
-        if(target < 1 || target > 99) throw new IllegalArgumentException("목표 체력은 1 이상 99 이하여야 합니다.");
+    public SimulationResult simulate(UserState userState, int targetStamina){
+        if(targetStamina < 1 || targetStamina > 99) throw new IllegalArgumentException("목표 체력은 1 이상 99 이하여야 합니다.");
+
+        switch (userState.getDay()) {
+            case WEEKEND:
+                return simulateWeekend(userState, targetStamina);
+            case WEEKDAY_MON:
+            case WEEKDAY_OTHER:
+                // todo: 평일 시뮬레이션
+        }
+
+        return SimulationResult.failure();
+    }
+
+    private SimulationResult simulateWeekend(UserState userState, int targetStamina) {
+        for (Map.Entry<PlaceCategory, Place> entry : userState.getPlaces().entrySet()) {
+            List<Tick> combination = new ArrayList<>();
+            if (findPath(0, 100, targetStamina, entry.getKey(), entry.getValue().getActions(), combination)) {
+                return SimulationResult.success(combination);
+            }
+        }
+        return SimulationResult.failure();
+    }
+
+    private boolean findPath(int currentTick, int currentStamina, int targetStamina,
+                             PlaceCategory place, Map<ActionCategory, Integer> availableActions, List<Tick> combination) {
+        //System.out.println("진입: " + currentTick + ", " + currentStamina);
+        if(currentStamina == targetStamina){
+            return true;
+        }
+
+        for (Map.Entry<ActionCategory, Integer> entry : availableActions.entrySet()) {
+            ActionCategory action = entry.getKey();
+            int delta = entry.getValue();
+
+            int nextStamina = currentStamina + delta;
+            //System.out.println("반영: " + nextStamina + ", " + delta);
+            combination.add(new Tick(place, action));
+            if (findPath(currentTick + 1, nextStamina, targetStamina, place, availableActions, combination)) {
+                return true;
+            }
+
+            combination.remove(combination.size() - 1);
+        }
+
+        return false;
     }
 }
