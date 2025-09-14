7fda548b53c77ccb83621bc9b3f656efb36c8594
isExample <yeji0331@naver.com>
2025-09-15 00:12:22 +0900

    refactor: 불필요한 import 제거

--- src/main/java/com/example/suki/domain/simulation/algorithm/AlgorithmStrategy.java
+++ src/main/java/com/example/suki/domain/simulation/algorithm/AlgorithmStrategy.java
@@ -1,11 +1,6 @@
 package com.example.suki.domain.simulation.algorithm;
 
-import com.example.suki.domain.User.UserState;
 import com.example.suki.domain.action.ActionCategory;
-import com.example.suki.domain.place.PlaceCategory;
-import com.example.suki.domain.simulation.DaySchedule;
-import com.example.suki.domain.simulation.goal.Goal;
-import com.example.suki.domain.simulation.model.ConsumableBag;
 import com.example.suki.domain.simulation.model.SimulationContext;
 import com.example.suki.domain.simulation.model.Tick;
 
