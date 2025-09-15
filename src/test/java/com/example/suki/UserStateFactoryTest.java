4a0f1516dccfadb341dfb09701a60d4d1b973da8
isExample <yeji0331@naver.com>
2025-09-15 17:01:46 +0900

    test: UserContext 직접 생성
    - SimulationRequest 직접 생성시 테스트에 불필요한 데이터 다량 포함
    - given 과정 간소화

--- src/test/java/com/example/suki/UserStateFactoryTest.java
+++ src/test/java/com/example/suki/UserStateFactoryTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -21,20 +20,13 @@ public class UserStateFactoryTest {
 
     @Test
     void 장소를_올바르게_활성화_및_비활성화시킨다() {
-        SimulationRequest request = new SimulationRequest(
-                60,
-                5,
+        UserContext context = new UserContext(
                 DayCategory.WEEKDAY_OTHER,
                 List.of(PlaceCategory.HOME), // 비활성화할 장소
-                List.of(PlaceCategory.GOLD_MINE), // 활성화할 장소
-                List.of(), // badgeList
-                List.of(), // traitList
-                List.of(), // permanentItemList
-                Map.of()   // consumableItemMap
+                List.of(PlaceCategory.GOLD_MINE) // 활성화할 장소
         );
-        UserContext userContext = UserContext.from(request);
 
-        UserState actualUserState = userStateFactory.create(userContext);
+        UserState actualUserState = userStateFactory.create(context);
 
         // 비활성화된 장소가 실제로 없는지 확인
         assertThat(actualUserState.getPlaces())
@@ -50,20 +42,13 @@ public class UserStateFactoryTest {
 
     @Test
     void 활성화_및_비활성화_목록이_비어있다면_기본_장소만_존재한다() {
-        SimulationRequest request = new SimulationRequest(
-                60,
-                5,
+        UserContext context = new UserContext(
                 DayCategory.WEEKDAY_OTHER,
                 List.of(), // 비활성화할 장소
-                List.of(), // 활성화할 장소
-                List.of(),
-                List.of(),
-                List.of(),
-                Map.of()
+                List.of() // 활성화할 장소
         );
-        UserContext userContext = UserContext.from(request);
 
-        UserState actualUserState = userStateFactory.create(userContext);
+        UserState actualUserState = userStateFactory.create(context);
 
         Set<PlaceCategory> expectedDefaultPlaces = Arrays.stream(PlaceCategory.values())
                 .filter(PlaceCategory::isDefault)
